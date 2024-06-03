d9f4bb97eb5f05c2ad671d6f3fea9fec75ee2f58
Binnur KURT <deepcloudlabs@gmail.com>
2024-06-03 12:28:04 +0300

    initial commit

--- module00-study.js/exercise09.js
+++ module00-study.js/exercise09.js
@@ -1,9 +1,9 @@
 class Circle {
-    constructor(x, y, radius, color) {
+    constructor(x, y, radius, color, thickness) {
         this.x = x;
         this.y = y;
         this.radius = radius;
-        this.color = color;
+        this.style = { color: color, thickness: thickness};
 //        this.area = this.area.bind(this);
 //        this.circumference = this.circumference.bind(this);
 //        this.printInfo = this.printInfo.bind(this);
@@ -19,10 +19,13 @@ class Circle {
 
     printInfo = () => {
         console.log(this);
-       console.log(`Area of the circle is ${this.area()}`);
+        console.log(`Area of the circle is ${this.area()}`);
     }
 }
 
-let circle = new Circle(0, 0, 1, "Red");
-// circle.printInfo(); // --> printInfo(circle)
-setInterval(circle.printInfo, 3_000);
+let circle1 = new Circle(0, 0, 1, "Red", 10);
+let circle2 = {...circle1}; // shallow cloning the circle object
+circle2.style = {...circle1.style};
+circle2.style.thickness = 100;
+console.log(circle1);
+console.log(circle2);
