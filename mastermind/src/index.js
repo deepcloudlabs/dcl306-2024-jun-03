5a66b1aedd8290637c2ce65c8f3b78bd838b0bc0
Binnur KURT <deepcloudlabs@gmail.com>
2024-06-04 15:22:03 +0300

    Update index.js

--- mastermind/src/index.js
+++ mastermind/src/index.js
@@ -2,10 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Mastermind from "./mastermind";
 import "bootstrap/dist/css/bootstrap.css";
-
+import {Route, Routes} from "react-router";
+import PlayerLoses from "./loses/player-loses";
+import PlayerWins from "./wins/player-wins";
+import {BrowserRouter} from "react-router-dom";
+const routing =
+    <Routes>
+        <Route path="/" element={<Mastermind></Mastermind>} exact></Route>
+        <Route path="/loses" element={<PlayerLoses></PlayerLoses>}></Route>
+        <Route path="/wins" element={<PlayerWins></PlayerWins>}></Route>
+    </Routes>
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <>
-    <Mastermind />
-  </>
+  <BrowserRouter>
+      {routing}
+  </BrowserRouter>
 );
